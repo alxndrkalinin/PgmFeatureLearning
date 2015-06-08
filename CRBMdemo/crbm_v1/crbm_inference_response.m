f1a8687c29f02427ae5b7884176a5b8ffb5b6ad0
alxndrkalinin <alxndrkalinin@gmail.com>
2015-06-08 15:23:18 -0400

    fix(response): take only gpuMode instead of all model parameters

--- CRBMdemo/crbm_v1/crbm_inference_response.m
+++ CRBMdemo/crbm_v1/crbm_inference_response.m
@@ -2,29 +2,32 @@
 %%% of convolutional restricted Boltzmann machine
 %%% with probabilistic max-pooling
 
-function [H HP Hc HPc] = crbm_inference_response(gpuMode, vis, CRBM, sigma, spacing)
+function [H, HP, Hc, HPc] = crbm_inference_response(gpuMode, vis, CRBM, sigma, spacing)
 %
 if ~exist('sigma','var') || isempty(sigma), 
     sigma = 1;
 end
 
-% transfer data to GPU
-if params.gpu ~= 0
-    CRBM.W = gpuArray(CRBM.W);
-    vis = gpuArray(vis);
-    hidprobs = zeros(size(vis,1)-size(CRBM.W,1)+1, ...
-        size(vis,2)-size(CRBM.W,2)+1, size(vis,3)-size(CRBM.W,3)+1, numhid, 'single', 'gpuArray');
-else
-    hidprobs = zeros(size(vis,1)-size(CRBM.W,1)+1, ...
-        size(vis,2)-size(CRBM.W,2)+1, size(vis,3)-size(CRBM.W,3)+1, numhid, 'single');
-end
-
 numvis = size(vis, 4);
 numhid = size(CRBM.W, 5);
 
+% transfer data to GPU
+if gpuMode ~= 0
+    CRBM.W = gpuArray(CRBM.W);
+    vis = gpuArray(vis);
+    hidprobs = zeros(size(vis ,1) - size(CRBM.W, 1) + 1, ...
+        size(vis, 2) - size(CRBM.W, 2) + 1, size(vis, 3) - size(CRBM.W, 3) + 1, ...
+        numhid, 'single', 'gpuArray');
+else
+    hidprobs = zeros(size(vis, 1) - size(CRBM.W, 1) + 1, ...
+        size(vis, 2) - size(CRBM.W, 2) + 1, size(vis, 3) - size(CRBM.W, 3) + 1, ...
+        numhid, 'single');
+end
+
 for b = 1:numhid,
     for c = 1:numvis,
-        hidprobs(:, :,:,b) = hidprobs(:,:,:,b) + convn(vis(:,:,:,c), CRBM.W(end:-1:1, end:-1:1, end:-1:1, c, b), 'valid');
+        hidprobs(:, :,:,b) = hidprobs(:,:,:,b) + ...
+            convn(vis(:,:,:,c), CRBM.W(end:-1:1, end:-1:1, end:-1:1, c, b), 'valid');
     end
     hidprobs(:,:,:,b) = hidprobs(:,:,:,b) + CRBM.hbias(b);
 end
@@ -33,7 +36,7 @@ clear vis CRBM;
 hidprobs = 1 / (sigma^2) .* hidprobs;
 
 % gather data from GPU
-if params.gpu ~= 0
+if gpuMode ~= 0
     hidprobs = gather(hidprobs);
 end
 
