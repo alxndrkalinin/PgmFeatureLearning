cfb389ede2b52e1bd5ebca473b1fabbb0e6b0a2b
alxndrkalinin <alxndrkalinin@gmail.com>
2015-06-15 19:57:02 -0400

    fix(vishidprob): do correct pre-allocation without overwriting probs if they exist

--- CRBMdemo/crbm_vishidprod.m
+++ CRBMdemo/crbm_vishidprod.m
@@ -3,36 +3,39 @@
 function PAR = crbm_vishidprod(PAR, params, opt)
 if ~exist('opt','var'), opt = 'pos'; end
 
+selidx1 = size(PAR.hidprobs, 1):-1:1;
+selidx2 = size(PAR.hidprobs, 2):-1:1;
+selidx3 = size(PAR.hidprobs, 3):-1:1;
+
 % transfer data to GPU
 if params.gpu ~= 0
     PAR.hidprobs = gpuArray(PAR.hidprobs);
     if strcmp(opt, 'pos')
         PAR.vis = gpuArray(PAR.vis);
-        if isfield(PAR, 'posprods')
-            PAR.posprods = gpuArray(PAR.posprods);
-        end
     elseif strcmp(opt, 'neg')
         PAR.negdata = gpuArray(PAR.negdata);
-        if isfield(PAR, 'negprods')
-            PAR.negprods = gpuArray(PAR.negprods);
-        end
     end
 end
 
-selidx1 = size(PAR.hidprobs, 1):-1:1;
-selidx2 = size(PAR.hidprobs, 2):-1:1;
-selidx3 = size(PAR.hidprobs, 3):-1:1;
-
-if strcmp(opt,'pos'),
-    
-    nX = max(size(PAR.vis, 1) - ...
-        size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 1) + 1, 0);
-    nY = max(size(PAR.vis, 2) - ...
-        size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 2) + 1, 0);
-    nZ = max(size(PAR.vis, 3) - ...
-        size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 3) + 1, 0);
-
-    PAR.posprods = zeros(nX, nY, nZ, params.numvis, params.numhid);
+if strcmp(opt, 'pos'),
+    if isfield(PAR, 'posprods')
+        if params.gpu ~= 0
+            PAR.posprods = gpuArray(PAR.posprods);
+        end
+    else
+        nX = max(size(PAR.vis, 1) - ...
+            size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 1) + 1, 0);
+        nY = max(size(PAR.vis, 2) - ...
+            size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 2) + 1, 0);
+        nZ = max(size(PAR.vis, 3) - ...
+            size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 3) + 1, 0);
+        if params.gpu ~= 0
+            PAR.posprods = zeros(nX, nY, nZ, params.numvis, params.numhid, ...
+                'single', 'gpuArray');  
+        else
+            PAR.posprods = zeros(nX, nY, nZ, params.numvis, params.numhid);
+        end
+    end
     
     %%% --- positive phase --- %%%
     for c = 1:params.numvis,
@@ -41,15 +44,24 @@ if strcmp(opt,'pos'),
         end
     end
 elseif strcmp(opt,'neg'),
-    
-    nX = max(size(PAR.negdata, 1) - ...
-        size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 1) + 1, 0);
-    nY = max(size(PAR.negdata, 2) - ...
-        size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 2) + 1, 0);
-    nZ = max(size(PAR.negdata, 3) - ...
-        size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 3) + 1, 0);
-
-    PAR.negprods = zeros(nX, nY, nZ, params.numvis, params.numhid);
+    if isfield(PAR, 'negprods')
+        if params.gpu ~= 0
+            PAR.negprods = gpuArray(PAR.negprods);
+        end
+    else
+        nX = max(size(PAR.negdata, 1) - ...
+            size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 1) + 1, 0);
+        nY = max(size(PAR.negdata, 2) - ...
+            size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 2) + 1, 0);
+        nZ = max(size(PAR.negdata, 3) - ...
+            size(PAR.hidprobs(selidx1, selidx2, selidx3, :), 3) + 1, 0);
+        if params.gpu ~= 0
+            PAR.negprods = zeros(nX, nY, nZ, params.numvis, params.numhid, ...
+                'single', 'gpuArray');
+        else
+            PAR.negprods = zeros(nX, nY, nZ, params.numvis, params.numhid);
+        end
+    end
     
     %%% --- negative phase --- %%%
     for c = 1:params.numvis,
